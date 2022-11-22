a77778ffc4794e47c0a5421c6e540a4d59939c2c
HarshaSalih <harshasalih2000@gmail.com>
2022-11-22 12:00:04 +0530

    commits

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,53 @@
-public class Main
+import java.beans.Customizer;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Customer
 {
-    public static void main(String[] args)
+    int tea=10;
+    int coffee=10;
+    int snacks=10;
+    int idli=8;
+    int dosha=6;
+
+    Customer()
     {
-        System.out.println("Hello world!");
+
+    }
+    public void tea(int count)
+    {
+        int prize=count*tea;
+    }
+    public void coffee(int count)
+    {
+        int prize=count*coffee;
+    }
+    public void snacks(int count)
+    {
+        int prize=count*snacks;
+    }
+    public void idli(int count)
+    {
+        int prize=count*idli;
+    }
+    public void dosha(int count)
+    {
+        int prize=count*dosha;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Customer> arrCus = new ArrayList<Customer>();
+        while (true) {
+            System.out.println("Select an option\n1.Generate bill\n2.View all transactions\n3.Exit\n");
+            Scanner sc = new Scanner(System.in);
+            int option = sc.nextInt();
+            if (option == 3) {
+                System.out.println("You are exiting from the program");
+                System.exit(0);
+            } else {
+            }
+            }
+
     }
 }
